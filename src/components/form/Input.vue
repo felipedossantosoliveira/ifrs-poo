18cad801eb6042ceab75596631e598889d4e9787
Felipe dos Santos Oliveira <felipedossantosads@gmail.com>
2024-05-29 22:37:28 +0000

    feat: adicionando componentes

---
+++ src/components/form/Input.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+const props = defineProps({
+  type: String,
+  placeholder: String
+})
+
+const value = defineModel()
+
+</script>
+<template>
+  <input class="py-2 px-4 rounded border shadow" :type="props.type" :placeholder="props.placeholder" v-model="value"/>
+</template>
